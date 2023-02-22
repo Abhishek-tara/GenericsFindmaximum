6a182d583f939c9759303da3e432aefd6a0d3bbc
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-22 10:53:39 +0530

    Added Main Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsFindMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Program to find the Maximum problem using Generics");
+        }
+    }
+}
