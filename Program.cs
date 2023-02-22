f4fe8d0b2d6e4151b8c2cc794ab91987a1f8e640
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-22 11:16:08 +0530

    Added program to find the Max Float using CompareTo method

--- Program.cs
+++ Program.cs
@@ -6,20 +6,20 @@
         {
             Console.WriteLine("Welcome to the Program to find the Maximum problem using Generics");
 
-            Console.WriteLine("Executing three test cases to find the maximum numbe among three numbers");
+            Console.WriteLine("Executing three test cases to find the maximum Float number among three numbers");
 
             Console.WriteLine("\nTest case 1: ");
-            int max1 = MaxInteger.FindMaxInt(80, 45, 60);
+            float max1 = MaxFloat.FindMaxFloat(6.5f, 4.5f, 3.2f);
             Console.WriteLine($"{max1} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 2: ");
-            int max2 = MaxInteger.FindMaxInt(40, 70, 30);
+            float max2 = MaxFloat.FindMaxFloat(4.1f, 7.2f, 3.3f);
             Console.WriteLine($"{max2} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 3: ");
-            int max3 = MaxInteger.FindMaxInt(20, 45, 50);
+            float max3 = MaxFloat.FindMaxFloat(2.2f, 4.5f, 5.6f);
             Console.WriteLine($"{max3} is the greatest among three number");
             
 
