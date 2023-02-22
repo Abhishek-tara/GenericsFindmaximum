1120fb7e316f810076205c18fe96f412fb70fe8d
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-22 11:08:15 +0530

    Added program to find the max Int using CompareTo method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Program to find the Maximum problem using Generics");
+
+            Console.WriteLine("Executing three test cases to find the maximum numbe among three numbers");
+
+            Console.WriteLine("\nTest case 1: ");
+            int max1 = MaxInteger.FindMaxInt(80, 45, 60);
+            Console.WriteLine($"{max1} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 2: ");
+            int max2 = MaxInteger.FindMaxInt(40, 70, 30);
+            Console.WriteLine($"{max2} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 3: ");
+            int max3 = MaxInteger.FindMaxInt(20, 45, 50);
+            Console.WriteLine($"{max3} is the greatest among three number");
+            
+
         }
     }
 }
