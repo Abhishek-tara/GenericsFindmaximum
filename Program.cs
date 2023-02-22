08e2b50f68422875cedb4893c93120930ee8fba1
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-22 12:51:05 +0530

    Refactor the program to find the Max String using Generic Method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Program to find the Maximum problem using Generics");
+
+            Console.WriteLine("\nExecuting three test cases to find the maximum value among three values");
+
+            Console.WriteLine("\nTest case 1: ");
+            int max1 = GenericMethod.FindMaxValue<int>(80, 45, 60);
+            Console.WriteLine($"{max1} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 2: ");
+            float max2 = GenericMethod.FindMaxValue<float>(4.5f, 7.5f, 3.3f);
+            Console.WriteLine($"{max2} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 3: ");
+            string max3 = GenericMethod.FindMaxValue<string>("Peach", "Apple", "Orange");
+            Console.WriteLine($"{max3} is the greatest among three number");
         }
     }
 }
